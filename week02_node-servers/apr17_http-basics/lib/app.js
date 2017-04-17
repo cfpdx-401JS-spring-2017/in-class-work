b70684dd949e5814affffa1b8d51e59488fc9dc2
martypdx <marty@martypdx.com>
2017-04-17 11:29:58 -0700

    refactor routes to dictionary object

--- week02_node-servers/apr17_http-basics/lib/app.js
+++ week02_node-servers/apr17_http-basics/lib/app.js
@@ -1,16 +1,37 @@
 
+function hello(req, res) {
+    res.end('hello world');
+}
+
+function goodbye(req, res) {
+    res.end('goodbye world');
+}
+
+const routes = {
+    '/hello': hello,
+    '/goodbye': goodbye
+};
+
 function app(req, res) {
     console.log(req.method, req.url);
-    if(req.url === '/hello') {
-        res.end('hello world');
-    }
-    else if(req.url === '/goodbye') {
-        res.end('goodbye world');
-    }
-    else {
+    const route = routes[req.url] || notFound;
+    route(req, res);
+}
+
+module.exports = app;
+
+
+
+function app(req, res) {
+    console.log(req.method, req.url);
+    const route = routes[req.url];
+    if(!route) {
         res.statusCode = 404;
         res.statusMessage = `${req.url} not found`;
         res.end();
+    }
+    else {
+        route(req, res);
     }
 }
 
