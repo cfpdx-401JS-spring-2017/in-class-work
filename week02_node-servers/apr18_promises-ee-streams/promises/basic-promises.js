6eaad1c3f0730162167c83ea2fcbadfc0757cacd
martypdx <marty@martypdx.com>
2017-04-18 11:50:18 -0700

    copyFile function

--- week02_node-servers/apr18_promises-ee-streams/promises/basic-promises.js
+++ week02_node-servers/apr18_promises-ee-streams/promises/basic-promises.js
@@ -1,10 +1,13 @@
 const fsp = require('fs-promise');
 
-fsp.readFile('foo.txt', 'utf-8')
-    .then(contents => {
-        console.log('first promise file contents', contents);
-        return fsp.readFile(contents, 'utf-8');
-    })
-    .then(contents => {
-        console.log('second promise file contents', contents);
-    });
+function copy(source, dest) {
+    return fsp.readFile(source)
+        .then(contents => {
+            return fsp.writeFile(dest, contents);
+        });
+}
+
+copy('bar.txt', 'qux.txt')
+    .then(() => {
+        console.log('copy file complete');
+    });    ;
