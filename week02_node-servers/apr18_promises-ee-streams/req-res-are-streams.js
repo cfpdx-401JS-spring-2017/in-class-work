b6f4e1cf9e93c0b18bdf1d066f65511f53051fd9
martypdx <marty@martypdx.com>
2017-04-18 11:20:28 -0700

    add a big file to stream back via pipe

--- week02_node-servers/apr18_promises-ee-streams/req-res-are-streams.js
+++ week02_node-servers/apr18_promises-ee-streams/req-res-are-streams.js
@@ -8,12 +8,17 @@ const server = http.createServer((req, res) => {
 
     const url = parseUrl(req.url, true);
     
-    if (url.pathname === '/' || url.pathname === '/index.html' ) {
+    if (url.pathname === '/big.pdf') {
+        const stream = fs.createReadStream('big.pdf');
+        res.setHeader('Content-Type', 'application/pdf');
+        stream.pipe(res);
+        return;
+    }
+
+    if (url.pathname === '/' || url.pathname === '/index.html') {
         const stream = fs.createReadStream('index.html');
         res.setHeader('Content-Type', 'text/html');
-
         stream.pipe(res);
-
         return;
     }
     
