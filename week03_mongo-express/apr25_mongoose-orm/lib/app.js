986cfa450f4a67695ec46f2e45b809ea8d4638db
martypdx <marty@martypdx.com>
2017-04-25 12:01:36 -0700

    prettier formatting

--- week03_mongo-express/apr25_mongoose-orm/lib/app.js
+++ week03_mongo-express/apr25_mongoose-orm/lib/app.js
@@ -12,31 +12,30 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('hello world');
+    res.send('hello world');
 });
 
 app.get('/unicorns/:id', (req, res) => {
-  const _id = new ObjectId(req.params.id);
-  connection.db.collection('unicorns')
-    .findOne({ _id })
-    .then(unicorn => {
-      if(!unicorn) {
-        res.status(404).send({error: 'resource not found'});
-      } else {
-        console.log(unicorn);
-        res.send(unicorn);
-      }
+    const _id = new ObjectId(req.params.id);
+    connection.db.collection('unicorns').findOne({ _id }).then(unicorn => {
+        if (!unicorn) {
+            res.status(404).send({ error: 'resource not found' });
+        } else {
+            console.log(unicorn);
+            res.send(unicorn);
+        }
     });
 });
 
 app.post('/unicorns', (req, res) => {
-  connection.db.collection('unicorns')
-    .insert(req.body)
-    .then(response => {
-      return response.ops[0];
-    })
-    .then(savedUnicorn => res.send(savedUnicorn))
-    .catch(err => console.log(err));
+    connection.db
+        .collection('unicorns')
+        .insert(req.body)
+        .then(response => {
+            return response.ops[0];
+        })
+        .then(savedUnicorn => res.send(savedUnicorn))
+        .catch(err => console.log(err));
 });
 
 module.exports = app;
