4cde4b25e0cbaa15be50603e377e8262aeb62be4
martypdx <marty@martypdx.com>
2017-04-25 10:00:57 -0700

    add file stream of file

--- week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/lib/app.js
+++ week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/lib/app.js
@@ -1,6 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 
-function app(req, res) { 
+const superCat = JSON.stringify({
+    name: 'super cat',
+    type: 'top secret'
+});
 
+const catHtml = path.join(__dirname, 'cat.html');
+
+function app(req, res) {
+    if (req.url === '/cat') {
+        res.setHeader('Content-Type', 'application/json');
+        res.end(superCat);
+    }
+    else {
+        fs.createReadStream(catHtml).pipe(res);
+    }
 }
 
 module.exports = app;
