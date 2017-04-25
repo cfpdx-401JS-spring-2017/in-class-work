a3d7d80140044f32db296b9ababb73ca31f6dffc
martypdx <marty@martypdx.com>
2017-04-25 10:18:18 -0700

    add 404 functionality for any other routes

--- week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/lib/app.js
+++ week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/lib/app.js
@@ -9,6 +9,13 @@ const superCat = JSON.stringify({
 const catHtml = path.join(__dirname, 'cat.html');
 
 function app(req, res) {
+    if (req.method !== 'GET') {
+        res.statusCode = 404;
+        res.statusMessage = 'why u no cat?';
+        res.end('<h2>please cat</h2>');
+        return;
+    }
+
     if (req.url === '/cat') {
         res.setHeader('Content-Type', 'application/json');
         res.end(superCat);
--- week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/test/http.test.js
+++ week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/test/http.test.js
@@ -6,15 +6,35 @@ const app = require('../lib/app');
 
 const request = chai.request(app);
 
-it('returns super cat', () => {
-    const superCat = { name: 'super cat', type: 'top secret' };
+describe('http pop quiz', () => {
+    it('returns super cat', () => {
+        const superCat = { name: 'super cat', type: 'top secret' };
 
-    return request.get('/cat')
-        .then(res => {
-            // superagent (chai-http) _if_ content type
-            // is `application/json`, will deserialize
-            // res.text to res.body
-            assert.deepEqual(res.body, superCat);
-        });
+        return request.get('/cat')
+            .then(res => {
+                // superagent (chai-http) _if_ content type
+                // is `application/json`, will deserialize
+                // res.text to res.body
+                assert.deepEqual(res.body, superCat);
+            });
+    });
 
+    it('servers cat html for other GET', () => {
+        return request.get('/')
+            .then(res => {
+                assert.equal(res.text, '<h1>Super Cat FTW!</h1>');
+            });
+    });
+
+    it('returns 404 for none GET', () => {
+        return request.post('/')
+            .then(() => {
+                throw new Error('should not succeed, 404 expected');
+            }, res => {
+                assert.equal(res.status, 404);
+                // TODO: superagent bug? investigate
+                // assert.equal(res.statusMessage, 'why u no cat?');
+                assert.equal(res.text, '<h2>please cat</h2>');
+            });
+    });
 });
