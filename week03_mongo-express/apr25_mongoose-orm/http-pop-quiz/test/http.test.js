c580d747c95d94a2c0904470ca61b34302b20b7d
martypdx <marty@martypdx.com>
2017-04-25 09:40:25 -0700

    reduce to single promise version

--- week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/test/http.test.js
+++ week03_mongo-express/apr25_mongoose-orm/http-pop-quiz/test/http.test.js
@@ -5,27 +5,15 @@ const assert = chai.assert;
 
 const request = chai.request(app);
 
-it('returns super cat', done => {
-
-    request.get('/cat')
-        .end((err, res) => {
-            if (err) return done(err);
-            // eslint-disable-next-line
-            assert.equal(res.text, `{ name: 'super cat', type: 'top secret' }`);
-            // same as above, but objectified
-            assert.deepEqual(res.body, superCat);
-            done();
-        });
-
-});
-
 it('returns super cat', () => {
     const superCat = { name: 'super cat', type: 'top secret' };
 
     return request.get('/cat')
         .then(res => {
-            // eslint-disable-next-line
-            assert.equal(res.text, `{ name: 'super cat', type: 'top secret' }`);
+            // superagent (chai-http) _if_ content type
+            // is `application/json`, will deserialize
+            // res.text to res.body
+            assert.deepEqual(res.body, superCat);
         });
 
 });
