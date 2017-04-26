0c33b963182552389620b4078ef0bb95420e17c8
martypdx <marty@martypdx.com>
2017-04-26 11:00:51 -0700

    organize model tests

--- week03_mongo-express/apr26_router-model/test/unit/pet.test.js
+++ week03_mongo-express/apr26_router-model/test/unit/pet.test.js
@@ -1,7 +1,8 @@
 const assert = require('chai').assert;
 const Pet = require('../../lib/models/pet');
 
-const expectedValidation = () => { throw new Error('expected validation errors'); }
+const expectedValidation = () => { throw new Error('expected validation errors'); };
+
 describe('pets model', () => {
 
     it('validates good model', () => {
@@ -12,46 +13,49 @@ describe('pets model', () => {
         return pet.validate();
     });
     
-    it('validates name and legs are required', () => {
-        const pet = new Pet();
-        return pet.validate()
-            .then(expectedValidation,
+    describe('validation failures', () => {
+
+        it('name and legs are required', () => {
+            const pet = new Pet();
+            return pet.validate()
+                .then(expectedValidation,
                 err => {
                     const errors = err.errors;
                     assert.ok(errors.legs && errors.legs.kind === 'required');
                     assert.ok(errors.name && errors.name.kind === 'required');
                 }
-            );
-    });
-
-    it('has at least 0 legs', () => {
-        const pet = new Pet({
-            name: 'pet',
-            legs: -1
+                );
         });
 
-        return pet.validate()
-            .then(expectedValidation,
+        it('has at least 0 legs', () => {
+            const pet = new Pet({
+                name: 'pet',
+                legs: -1
+            });
+
+            return pet.validate()
+                .then(expectedValidation,
                 err => {
                     const errors = err.errors;
                     assert.ok(errors.legs && errors.legs.kind === 'min');
                 }
-            );
-    });
-
-    it('has at no more than 8 legs', () => {
-        const pet = new Pet({
-            name: 'pet',
-            legs: 9
+                );
         });
 
-        return pet.validate()
-            .then(expectedValidation,
+        it('has at no more than 8 legs', () => {
+            const pet = new Pet({
+                name: 'pet',
+                legs: 9
+            });
+
+            return pet.validate()
+                .then(expectedValidation,
                 err => {
                     const errors = err.errors;
                     assert.ok(errors.legs && errors.legs.kind === 'max');
                 }
-            );
-    });
+                );
+        });
+    });    
 });
 
