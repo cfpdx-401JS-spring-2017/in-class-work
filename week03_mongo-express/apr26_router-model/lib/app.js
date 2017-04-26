73d48f39a10a5f6d9dc3b9254554905a0172a44a
martypdx <marty@martypdx.com>
2017-04-26 11:22:37 -0700

    simple examples of next() in middleware

--- week03_mongo-express/apr26_router-model/lib/app.js
+++ week03_mongo-express/apr26_router-model/lib/app.js
@@ -2,7 +2,25 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan('dev'));
+// example of "logging" type middleware that
+// does passive work without being involved in 
+// request or response
+app.use((req, res, next) => {
+    console.log(`${req.method} to path ${req.url}`);
+    next();
+});
+
+// append additional request information
+// to the request object
+// (bodyparser, authorization/authentication)
+app.use((req, res, next) => {
+    req.foo = 'FOO';
+    next();
+});
+
+app.get('/test', (req, res) => {
+    res.send(req.foo);
+});
 
 app.get('/api/pets', (req, res) => {
     res.send([]);
