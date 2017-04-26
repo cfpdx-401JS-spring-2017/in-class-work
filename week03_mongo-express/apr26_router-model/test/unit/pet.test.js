0dfdee5bf088a91e4387c3e68efc6e6b1dba698e
martypdx <marty@martypdx.com>
2017-04-26 10:50:49 -0700

    pet model validation

--- week03_mongo-express/apr26_router-model/test/unit/pet.test.js
+++ week03_mongo-express/apr26_router-model/test/unit/pet.test.js
@@ -1,7 +1,49 @@
 const assert = require('chai').assert;
+const Pet = require('../../lib/models/pet');
 
+const expectedValidation = () => { throw new Error('expected validation errors'); }
 describe('pets model', () => {
-    it('works', () => {
-        assert.ok(true);
+    
+    it('validates name and legs are required', () => {
+        const pet = new Pet();
+        return pet.validate()
+            .then(expectedValidation,
+                err => {
+                    const errors = err.errors;
+                    assert.ok(errors.legs && errors.legs.kind === 'required');
+                    assert.ok(errors.name && errors.name.kind === 'required');
+                }
+            );
+    });
+
+    it('has at least 0 legs', () => {
+        const pet = new Pet({
+            name: 'pet',
+            legs: -1
+        });
+
+        return pet.validate()
+            .then(expectedValidation,
+                err => {
+                    const errors = err.errors;
+                    assert.ok(errors.legs && errors.legs.kind === 'min');
+                }
+            );
+    });
+
+    it('has at no more than 8 legs', () => {
+        const pet = new Pet({
+            name: 'pet',
+            legs: 9
+        });
+
+        return pet.validate()
+            .then(expectedValidation,
+                err => {
+                    const errors = err.errors;
+                    assert.ok(errors.legs && errors.legs.kind === 'max');
+                }
+            );
     });
 });
+
