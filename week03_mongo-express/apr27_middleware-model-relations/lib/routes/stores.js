4b2e3472fa5116ad9ec8f47d8029f22691cd930d
martypdx <marty@martypdx.com>
2017-04-27 12:19:01 -0700

    add child data to parent object on get

--- week03_mongo-express/apr27_middleware-model-relations/lib/routes/stores.js
+++ week03_mongo-express/apr27_middleware-model-relations/lib/routes/stores.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router;
 const router = Router();
 const Store = require('../models/store');
+const Pet = require('../models/pet');
 
 router
     .post('/', (req, res, next) => {
@@ -8,6 +9,22 @@ router
             .save()
             .then(store => res.send(store))
             .catch(next);
+    })
+    
+    .get('/:id', (req, res, next) => {
+        const storeId = req.params.id;
+        Promise.all([
+            Store.findById(storeId).lean(),
+            Pet.find({ store: storeId }).lean()
+        ])
+            .then(result => {
+                const store = result[0];
+                const pets = result[1];
+                store.pets = pets;
+                res.send(store);
+            })
+            .catch(next);
+            
     });
 
 
