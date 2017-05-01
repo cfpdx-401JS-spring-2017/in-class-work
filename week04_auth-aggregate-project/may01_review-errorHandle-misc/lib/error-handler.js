9c45e716f64211deb39a12fcd768a5e1ecd23b72
martypdx <marty@martypdx.com>
2017-05-01 09:39:48 -0700

    add commented out refactor to stategy pattern

--- week04_auth-aggregate-project/may01_review-errorHandle-misc/lib/error-handler.js
+++ week04_auth-aggregate-project/may01_review-errorHandle-misc/lib/error-handler.js
@@ -38,3 +38,52 @@ function getErrorHandler() {
 }
 
 module.exports = getErrorHandler;
+
+
+// function getErrorHandler() {
+//     // eslint-disable-next-line no-unused-vars    
+//     return function errorHandler(err, req, res, next) { 
+//         let handler = null;
+
+//         // Mongoose Validation and Cast Errors
+//         if (err.errors) {
+//             handler = mongooseError;
+//         }
+//         // One of our errors, with "code" property
+//         else if (err.code) {
+//             handler = customError;
+//         }
+//         // or something unexpected?
+//         else {
+//             handler = unknownError;
+//         }
+
+//         const { code, error } = handler(err);
+
+//         // send back code and error data        
+//         res.status(code).send({ error });
+//     };  
+// }
+
+// module.exports = getErrorHandler;
+
+// function mongooseError(err) {
+//     const validations = err.errors;
+//     // create an array of all Mongoose errors
+//     const error = Object.keys(validations).reduce((messages, key) => {
+//         messages.push(validations[key].message);
+//         return messages;
+//     }, []);
+
+//     return { code: 400, error };
+// }
+
+// function customError(err) {
+//     return err;
+// }
+
+// function unknownError(err) {
+//     //eslint-disable-next-line no-console
+//     console.log(err);
+//     return { code: 500, error: 'Internal Server Error' };
+// }
