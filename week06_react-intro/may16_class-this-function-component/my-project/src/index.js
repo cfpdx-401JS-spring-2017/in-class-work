f71533adddc92fc735338ce0735d583ff24c8a8a
martypdx <marty@martypdx.com>
2017-05-16 15:07:05 -0700

    Simple react "Pure Function" components example

--- week06_react-intro/may16_class-this-function-component/my-project/src/index.js
+++ week06_react-intro/may16_class-this-function-component/my-project/src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 
+function Simples(props) {
+  return <div>
+    {props.places.map(place =>
+      <Simple greeting={props.greeting} place={place} />
+    )}
+  </div>
+}
+function Simple({ greeting, place }) {
+  return <div>{greeting} {place}</div>;
+}
+
 ReactDOM.render(
-  <App />,
+  <Simples
+    places={['world', 'Portland', 'universe']}
+    greeting="Hola"/>,
   document.getElementById('root')
 );
