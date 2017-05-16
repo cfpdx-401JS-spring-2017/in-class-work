71847b9b5b793236530b9e1bc7c34c978a5a9cd4
martypdx <marty@martypdx.com>
2017-05-16 15:26:41 -0700

    Refactor NumberList

--- week06_react-intro/may16_class-this-function-component/my-project/src/App.js
+++ week06_react-intro/may16_class-this-function-component/my-project/src/App.js
@@ -2,62 +2,95 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+    constructor() {
+        super();
 
-  constructor() { 
-    super();
+        this.state = {
+            size: 20,
+            color: '#aaaaaa',
+            numbers: []
+        };
 
-    this.state = {
-      size: 20,
-      color: '#aaaaaa'
-    }    
+        this.changeFontSize = this.changeFontSize.bind(this);
+        this.changeColor = this.changeColor.bind(this);
+    }
 
-    this.changeFontSize = this.changeFontSize.bind(this);
-    this.changeColor = this.changeColor.bind(this);
-  }
+    changeFontSize({ target }) {
+        this.setState({ size: target.value });
+    }
 
-  changeFontSize({ target }) {
-    this.setState({ size: target.value });
-  }
+    changeColor(color) {
+        this.setState({ color });
+    }
+  
+    addNumber() {
+      const number = Math.floor(Math.random() * 10);
+      // Immumtable "way"
+      // const numbers = this.state.numbers.slice();
+      // numbers.push(number);
+      // But, for today...
+      const numbers = this.state.numbers;
+      numbers.push(number);
+      this.setState({ numbers });
+    }
 
-  changeColor(color) {
-    this.setState({ color });
-  }
+    render() {
+        const { size, color, numbers } = this.state;
 
-  render() {
-    const name = 'Ivy';
-    const numbers = [1, 3, 8];
-    const { size, color } = this.state;
-    // const size = this.state.size;
-    // const color = this.state.color;
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Numbers and Editor</h2>
+                </div>
+                <NumberList numbers={numbers} />
+                <button onClick={() => this.addNumber()}>Add a Number</button>
+                <div>
+                    size is {size}
+                    <input
+                        type="range"
+                        value={size}
+                        onChange={this.changeFontSize}
+                    />
+                    <br />
+                    <input
+                        type="color"
+                        value={color}
+                        onChange={this.changeColor}
+                    />
+                </div>
 
+                <p
+                    style={{
+                        fontSize: `${size}px`,
+                        color
+                    }}
+                >
+                    Here is some very nice text
+                </p>
+            </div>
+        );
+    }
+}
+
+function NumberList({ numbers }) {
     return (
-        <div className="App">
-          <div className="App-header">
-                <h2>{name}</h2>
-          </div>
-          <ul>
-          {numbers.map((number, i) => <li key={i}>{number}</li>)}
-          </ul>
-          <div>
-          size is {size}
-          <input type="range"
-            value={size}
-            onChange={this.changeFontSize} />
-          <br/>
-            <input type="color" 
-              value={color}
-              onChange={this.changeColor} />
-          </div>
-        
-          <p style={{
-                  fontSize: `${size}px`,
-                  color
-              }}>
-              Here is some very nice text
-          </p>
-        </div>
+        <ul>
+            {numbers.map((number, i) => <Number key={i} number={number}/>)}
+        </ul>
     );
-  }
+}
+
+function Number({ number }) {
+  const Component = number > 5 ? BigNumber : SmallNumber;
+  return <li><Component number={number}/></li>;
+}
+
+function SmallNumber({ number }) {
+  return <super>{number}</super>;
+}
+
+function BigNumber({ number }) {
+  return <strong>{number}</strong>;
 }
 
 export default App;
--- week06_react-intro/may16_class-this-function-component/my-project/src/index.js
+++ week06_react-intro/may16_class-this-function-component/my-project/src/index.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './App';
+import App from './App';
 import './index.css';
 
-function Simples(props) {
-  return <div>
-    {props.places.map(place =>
-      <Simple greeting={props.greeting} place={place} />
-    )}
-  </div>
-}
-function Simple({ greeting, place }) {
-  return <div>{greeting} {place}</div>;
-}
 
 ReactDOM.render(
-  <Simples
-    places={['world', 'Portland', 'universe']}
-    greeting="Hola"/>,
+  <App/>,
   document.getElementById('root')
 );
