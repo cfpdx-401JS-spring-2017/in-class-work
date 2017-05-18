2b0d619df2a827d0bba324315a2cd5f90f5d300f
martypdx <marty@martypdx.com>
2017-05-18 16:32:20 -0700

    final bits

--- week06_react-intro/may18_react-testing/snapshot-testing/src/Greetings.js
+++ week06_react-intro/may18_react-testing/snapshot-testing/src/Greetings.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 
-export function Greeter ({ name }) {
-  return <div>Hello <span>{name}</span></div>;
+export function Greeter({ salutation, name }) {
+  return <div>{salutation || 'Hello'} {name}</div>;
 }
 
 export class Greetings extends Component {
+  constructor() {
+    super();
+    this.state = {
+      salutation: 'Hello'
+    };
+  }
+
+  changeSalutation() { 
+    this.setState({
+      salutation: this.getOppositeSalutation()
+    });
+  }
+
+  getOppositeSalutation() {
+    return this.state.salutation === 'Hello' ? 'Goodbye' : 'Hello'
+  }
+
   render() {
+    const { salutation } = this.state;
     const { people } = this.props;
     return (
-      <ul>
-        {people.map((person, i) => <Greeter key={i} name={person}/>)}
-      </ul>
+      <div>
+        <ul>
+          {people.map((person, i) => (
+            <Greeter key={i} salutation={salutation} name={person} />
+          ))}
+        </ul>
+        <button onClick={() => this.changeSalutation()}>
+          Say {this.getOppositeSalutation()}
+        </button>
+      </div>
     );
   }
 }
