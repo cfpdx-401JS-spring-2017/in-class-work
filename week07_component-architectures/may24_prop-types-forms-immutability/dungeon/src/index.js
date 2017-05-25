5e77a5257a689fe324b8219c3f003f7dad60cea7
martypdx <marty@martypdx.com>
2017-05-25 09:03:00 -0700

    rename folders

--- week07_component-architectures/may24_prop-types-forms-immutability/dungeon/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DungeonApp from './DungeonApp';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<DungeonApp />, document.getElementById('root'));
-registerServiceWorker();
--- week07_component-architectures/may24_prop-types-forms-immutability/go-to-movies/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import MovieApp from './MovieApp';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
-
-ReactDOM.render(<MovieApp />, document.getElementById('root'));
-registerServiceWorker();
