9f087d7724a2d77259994a0df3c6cbc3271d4524
martypdx <marty@martypdx.com>
2017-05-31 13:41:36 -0700

    simplify to function

--- week08_routing-services-css/may31_fullstack/pet-store/app/src/stores/Stores.js
+++ week08_routing-services-css/may31_fullstack/pet-store/app/src/stores/Stores.js
@@ -3,24 +3,19 @@ import { Route } from 'react-router-dom';
 import Store from './Store';
 import StoreDetail from './StoreDetail';
 
-export default class Stores extends Component {
-  
-  render() {
-    const { stores, match } = this.props;
-
-    return (
-      <div>
-        <h2>All Our Pet Stores</h2>
-        <ul>
-          {stores.map(store => 
-            <Store 
-              key={store._id} 
-              to={`${match.url}/${store._id}`}
-              {...store}/>
-          )}
-        </ul>
-        <Route path={`${match.url}/:storeId`} component={StoreDetail}/>
-      </div>
-    );
-  }
+export default function Stores({ stores, match }) {
+  return (
+    <div>
+      <h2>All Our Pet Stores</h2>
+      <ul>
+        {stores.map(store => 
+          <Store 
+            key={store._id} 
+            to={`${match.url}/${store._id}`}
+            {...store}/>
+        )}
+      </ul>
+      <Route path={`${match.url}/:storeId`} component={StoreDetail}/>
+    </div>
+  );
 }
