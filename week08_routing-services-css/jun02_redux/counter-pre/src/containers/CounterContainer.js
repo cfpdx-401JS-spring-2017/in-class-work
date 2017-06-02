0670ea9bbf703d8f3a0e3e03f6503586c159c294
martypdx <marty@martypdx.com>
2017-06-02 12:42:34 -0700

    verify bindActionCreators includes params

--- week08_routing-services-css/jun02_redux/counter-pre/src/containers/CounterContainer.js
+++ week08_routing-services-css/jun02_redux/counter-pre/src/containers/CounterContainer.js
@@ -1,7 +1,7 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-// import * as actions from '../actions/counter.actions';
-import { increment as incr, decrement as decr } from '../actions/counter.actions';
+import * as actions from '../actions/counter.actions';
+// import { increment as incr, decrement as decr } from '../actions/counter.actions';
 import Counter from '../components/Counter';
 
 function mapStateToProps(state) {
@@ -10,16 +10,16 @@ function mapStateToProps(state) {
     };
 }
 
-// function mapDispatchToProps(dispatch) {
-//   return bindActionCreators(actions, dispatch);
-// }
-
 function mapDispatchToProps(dispatch) {
-  return {
-      increment(amount) { dispatch(incr(amount)) },
-      decrement(amount) { dispatch(decr(amount)) }
-  }
+  return bindActionCreators(actions, dispatch);
 }
+
+// function mapDispatchToProps(dispatch) {
+//   return {
+//       increment(amount) { dispatch(incr(amount)) },
+//       decrement(amount) { dispatch(decr(amount)) }
+//   }
+// }
 
 
 
