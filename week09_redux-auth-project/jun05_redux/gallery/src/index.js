72a4a010038e0484f46edcdbd3b3dab6e3dc2498
martypdx <marty@martypdx.com>
2017-06-05 15:12:17 -0700

    simple logger middleware

--- week09_redux-auth-project/jun05_redux/gallery/src/index.js
+++ week09_redux-auth-project/jun05_redux/gallery/src/index.js
@@ -5,7 +5,7 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 
-import './redux-examples/combine-reducers';
+import './redux-examples/simple-middleware';
 
 
 
