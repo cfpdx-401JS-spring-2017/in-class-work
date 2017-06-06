dcff525987189b00a4dd69d325664f300d8f95d9
martypdx <marty@martypdx.com>
2017-06-06 14:12:57 -0700

    put in notes for bindActionCreators

--- week09_redux-auth-project/jun06_redux-router/gallery/src/album-detail/AlbumDetailContainer.js
+++ week09_redux-auth-project/jun06_redux-router/gallery/src/album-detail/AlbumDetailContainer.js
@@ -14,6 +14,16 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return bindActionCreators(actions, dispatch);
 }
+// bindActionCreators turns an action creators like: 
+// getAlbum(id)
+// addImage(image)
+// removeImage(imageId)
+// into:
+// {
+//    getAlbum(id) { dispatch(getAlbum(id)) }
+//    addImage(image) { dispatch(addImage(image)) }
+//    removeImage(imageId) { dispatch(removeImage(imageId)) }
+// }
 
 const AlbumDetailContainer = connect(
   mapStateToProps, 
